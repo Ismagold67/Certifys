d544f857a81b626216a082a3792e963a51cd72b4
IsmaGold67 <ismaelroberto67@gmail.com>
2023-08-04 01:35:32 -0400

    change time setTimeout

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function msg(){
     Msg.style.display = 'flex'
     Msg.style.transition = '3s'
 }
-setTimeout(msg , 2000)
+setTimeout(msg , 1000)
 function msgClose(){
     const Msg = document.querySelector('.msg')
     Msg.style.display = 'none'
